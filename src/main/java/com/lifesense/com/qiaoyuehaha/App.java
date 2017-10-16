0235de05803ad3e284eae1f56ccf4a6e4f1cd855
qiaoyue.sun <qiaoyue.sun@lifesense.com>
2017-10-16 16:34:45 +0800

    qiaoyuetest

---
+++ src/main/java/com/lifesense/com/qiaoyuehaha/App.java
@@ -0,0 +1,13 @@
+package com.lifesense.com.qiaoyuehaha;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
